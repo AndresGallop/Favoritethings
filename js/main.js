0440332606c4ec1f77d10984cf82720a67abbe45
AndresGallop <andresgallod.i@gmail.com>
2020-11-10 16:08:23 -0500

    Lightbox created and functional

---
+++ js/main.js
@@ -0,0 +1,42 @@
+(()=> {
+    console.log('fired!');
+  
+    let lightbox = document.querySelector(".lightboxes"),
+        lbClose = lightbox.querySelector("span"),
+        //lbVideo = lightBox.querySelector("video"),
+  
+        things = document.querySelectorAll(".thing"),
+        //houseImages = document.querySelector("#houseImages"),
+        thequote = document.querySelector(".quote"),
+        thereco = document.querySelector(".recome");
+  
+  
+  
+  
+    function showHideLightbox() {
+
+        console.log('parcerito');
+      setTimeout(function(){lightbox.classList.toggle('show-lightbox');}, 500);
+  
+      //lbVideo.src = `video/House${this.dataset.vid}.mp4`;
+    
+  }
+  
+  
+//   function animateBanner() {
+  
+//     //and also show the house name
+//     thequote.textContent = `House ${houseInfo[this.dataset.offset][0]}`;
+//     thereco.textContent = `${houseInfo[this.dataset.offset][1]}`;
+//     //debugger;
+//   }
+  
+    //things.forEach(sigil => sigil.addEventListener("click", showHideLightbox));
+  
+//things.forEach(sigil => sigil.addEventListener("click", animateBanner));
+    things.forEach(sigil => sigil.addEventListener('click', showHideLightbox));
+  
+    lbClose.addEventListener("click", showHideLightbox);
+    // lbClose.addEventListener("click", again);
+  
+  })();
