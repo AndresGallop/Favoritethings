a8087c24a0253309718a2e64a1dd880129bc62b1
AndresGallop <andresgallod.i@gmail.com>
2020-11-13 19:09:07 -0500

    trying

--- js/main.js
+++ js/main.js
@@ -7,13 +7,15 @@ import { fetchData } from "./modules/DataMiner.js";
     console.log('loaded');
 
     let lightbox = document.querySelector(".lightbox"),
-        thingo = document.querySelectorAll(".thing"),
+        //lacosa = document.querySelectorAll(".thing"),
         lbClose = lightbox.querySelector("span");
 
     // this receives the data payload from our AJAX request, parses it (turns the returned JSON object back into a plain JavaScript object) and renders the data to our view (the markup in index.html)
     //  function handleDataSet(thumbs) {
             
     //  }
+
+    
 
     function retrieveProjectInfo(event){
         //test for an ID
@@ -26,16 +28,19 @@ import { fetchData } from "./modules/DataMiner.js";
         fetchData(`./includes/index.php?id=${event.target.id}`).then(data => showtext(data, userPieces)).catch(err => console.log(err));
      }
 
-    function showHideLightbox() {
+     function showHideLightbox() {
 
         console.log('parcerito');
       setTimeout(function(){lightbox.classList.toggle('show-lightbox');}, 200);
     
   }
 
+    
+
     function renderPortfolioThumbnails(thumbs) {
         let  userSection = document.querySelector('.things-section');
         let userTemplate = document.querySelector('#things-template').content;
+        let lacosa = document.querySelectorAll(".thing");
         //let template = document.querySelector('#user-template').children;
             
 
@@ -43,11 +48,14 @@ import { fetchData } from "./modules/DataMiner.js";
         for (let thing in thumbs) {
             let currentUser = userTemplate.cloneNode(true),
                 currentUserText = currentUser.querySelector('.thing').children;
+
             
 
-            currentUserText[1].innerHTML = thumbs[thing].Thing; 
+            currentUserText[0].innerHTML = thumbs[thing].Thing; 
             //currentUserText[1].src = `images/${thumbs[thing].avatar}`; 
             currentUserText[1].id = thumbs[thing].id;  // Esta mierda es para obtener los datos (se ven en la consola), no se ven en la pantalla.
+           // lacosa.style.background = `images/${thumbs[thing].Img}`;
+
 
              
 
@@ -89,7 +97,7 @@ import { fetchData } from "./modules/DataMiner.js";
 
     fetchData('./includes/index.php').then(data => renderPortfolioThumbnails(data)).catch(err => console.log(err));
 
-    thingo.forEach(sigil => sigil.addEventListener('click', showHideLightbox));
+    lacosa.forEach(sigil => sigil.addEventListener("click", showHideLightbox));
   
     lbClose.addEventListener("click", showHideLightbox);
 
